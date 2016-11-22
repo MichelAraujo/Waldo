a97c7077618f69c52409556e5f42f9b79c5a71d2
Michel Araujo <michel_araujo.ti@hotmail.com>
2016-11-21 22:54:47 -0200

    Feito alguns testes para manipulação de string

--- src/Waldo.js
+++ src/Waldo.js
@@ -1,6 +1,29 @@
 
 class Waldo {
 
+  constructor() {
+  	this.interaction = null;
+  }
+
+  setInteraction(interaction) {
+  	this.interaction = interaction;
+  }
+
+  getResponse() {
+    if (this.interaction.indexOf('Quem é') !== -1) {
+      const name = this.interaction.substr(6).trim();
+
+      if (name == 'Michel Araujo Pinto') {
+      	return 'E o meu Criador!';
+      } else {
+      	return 'É o criador do protocolo HTTP';
+      }
+    }
+
+  	if (this.interaction === 'oi') {
+  	  return 'Oi, como vai?';
+  	}
+  }
 }
 
 module.exports = Waldo;
--- tests/waldoTest.js
+++ tests/waldoTest.js
@@ -2,8 +2,27 @@ const assert = require('assert');
 const Waldo = require('../src/Waldo');
 
 describe('waldo', () => {
-  it('#Teste de instancia da classe', () => {
+  it('#Teste de instancia da classe', (done) => {
   	const waldo = new Waldo();
   	assert.ok(waldo instanceof Waldo);
+  	done();
+  });
+
+  it('#Teste se Waldo responde oi', (done) => {
+  	const waldo = new Waldo();
+  	waldo.setInteraction('oi');
+  	assert.equal(waldo.getResponse(), 'Oi, como vai?');
+  	done();
+  });
+
+  it('#Teste se Waldo reconhece alguém', (done) => {
+  	const waldo = new Waldo();
+  	waldo.setInteraction('Quem é Michel Araujo Pinto');
+  	assert.equal(waldo.getResponse(), 'E o meu Criador!');
+
+  	waldo.setInteraction('Quem é Tim Berners-Lee');
+  	assert.equal(waldo.getResponse(), 'É o criador do protocolo HTTP');
+
+  	done();
   });
 });
