1e7d5a5095b048ec60715f3628f4b2231ccbd9bc
Michel Araujo <michel_araujo.ti@hotmail.com>
2016-11-23 19:57:09 -0200

    Adicionado tasks no gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,25 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 const util = require('gulp-util');
+const jshint = require('gulp-jshint');
+const git = require('gulp-git');
+
+gulp.task('jshint', () => {
+  return gulp.src('src/**/*.js')
+  	.pipe(jshint())
+  	.pipe(jshint.reporter('default'))	
+});
 
 gulp.task('test', () => {
   return gulp.src(['tests/*.js'], { read: false })
     .pipe(mocha({ reporter: 'spec' }))
     .on('error', util.log);
 });
+
+gulp.task('push-dev', () => {
+  return git.push('origin', 'develop', (err) => {
+  	if (err) throw err;
+  });
+});
+
+gulp.task('default', ['jshint']);
